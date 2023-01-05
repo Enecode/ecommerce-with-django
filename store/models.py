ed7edd4053b7ea1330395f468a0ed7184162842b
Isah Jacob <isahjacob0@gmail.com>
2023-01-05 15:07:45 +0100

    collection class model with one-to-many relationship with customer, product, and order

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DecimalField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT())
 
 
 class Customer(models.Model):
@@ -40,6 +45,7 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class Address(models.Model):
