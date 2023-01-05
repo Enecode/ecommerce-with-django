3d81d75da93878db6856d9cb5aa2b712c08fc722
Isah Jacob <isahjacob0@gmail.com>
2023-01-05 11:58:19 +0100

    product model class

---
+++ store/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DecimalField(auto_now=True)
