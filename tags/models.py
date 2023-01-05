3cfe6f9d0dc9c86fce9e5dbfad37f1ffd4bb3342
Isah Jacob <isahjacob0@gmail.com>
2023-01-05 21:38:43 +0100

    tag class and tagged item class with a generic content type so every product and content type or customer can be tagged

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,14 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = models.GenericForeignKey()
+
