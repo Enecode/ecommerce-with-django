d3e5863bec197bf47a8c228f8cc0424b11dc837a
Isah Jacob <isahjacob0@gmail.com>
2023-01-05 15:47:39 +0100

    a cart class model which a foreign key in cart item model class
    order and product are both foreign key in order item model class because order can be made on different products

--- store/models.py
+++ store/models.py
@@ -53,8 +53,19 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT())
-    product = models.ForeignKey(Product, on_delete=models.PROTECT())
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField( max_digits=6, decimal_places=2)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
