d9792da880085efa3f1743803daed52177994c73
Isah Jacob <isahjacob0@gmail.com>
2023-01-05 13:15:01 +0100

    address class model with a one-to-one relationship with customer

--- store/models.py
+++ store/models.py
@@ -27,7 +27,6 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_STATUS_CHOICE, default=MEMBERSHIP_STATUS_BRONZE)
 
 
-
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -41,3 +40,9 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
